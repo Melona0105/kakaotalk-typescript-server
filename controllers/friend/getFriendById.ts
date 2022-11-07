36d2a786aa84472ba3cbd6efc1c5dac18451fbde
대왕더덕 <duduki@daewangdeodeog-ui-MacBookAir.local>
2022-11-08 02:04:08 +0900

    implemnets friend profile

---
+++ controllers/friend/getFriendById.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import { database } from "../../database/database";
+import { RESPONES_MESSAGE } from "../../utils/commonConstants";
+
+/**
+ * 친구의 이메일과 일치하는 데이터를 리턴합니다.
+ */
+function getFriendById(req: Request, res: Response) {
+  const { friendId } = req.params;
+
+  try {
+    // 주어진 이메일과 일치하는 데이터 쿼리
+    database.query(
+      `SELECT id, email, username, summary from users WHERE id="${friendId}"`,
+      (err, data) => {
+        if (err) {
+          console.log(err);
+
+          return res
+            .status(400)
+            .send({ message: RESPONES_MESSAGE.BAD_USER_INPUT });
+        }
+
+        return res.status(201).send(data?.[0]);
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .send({ message: RESPONES_MESSAGE.INTERNAL_SERVER_ERROR });
+  }
+}
+
+export default getFriendById;
--- routes/friend.ts
+++ routes/friend.ts
@@ -4,6 +4,7 @@ import blockFriend from "../controllers/friend/blockFriend";
 import deleteFriend from "../controllers/friend/deleteFriend";
 import getBlockedFriends from "../controllers/friend/getBlockedFriends";
 import getFriend from "../controllers/friend/getFriend";
+import getFriendById from "../controllers/friend/getFriendById";
 import getMyHiddenFriends from "../controllers/friend/getHiddenFriends";
 import getMyFriends from "../controllers/friend/getMyFriends";
 import hideFriend from "../controllers/friend/hideFriend";
@@ -12,6 +13,7 @@ import auth from "../middlewares/auth";
 
 const friendRouter = Router();
 
+friendRouter.get("/get/:friendId", auth, getFriendById);
 friendRouter.get("/hide", auth, getMyHiddenFriends);
 friendRouter.get("/block", auth, getBlockedFriends);
 friendRouter.get("/:email", auth, getFriend);
